d5b3771e9b485fe6744aa30a71bc3f42953d06ff
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-09 13:00:14 -0500

    Initial commit

---
+++ src/com/amdudda/CentralAC.java
@@ -0,0 +1,44 @@
+package com.amdudda;
+
+import java.util.Date;
+
+/**
+ * Represents a service call to fix a central AC until
+ */
+public class CentralAC extends ServiceCall{
+
+    private String model;
+
+
+    public CentralAC(String serviceAddress, String problemDescription, Date date, String model) {
+        super(serviceAddress, problemDescription, date);
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+
+    @Override
+    public String toString() {
+
+       String resolvedDateString = ( resolvedDate == null) ? "Unresolved" : this.resolvedDate.toString();
+        String resolutionString = ( this.resolution == null) ? "Unresolved" : this.resolution;
+        String feeString = (fee == UNRESOLVED) ? "Unresolved" : "$" + Double.toString(fee);
+
+
+        return "Central AC Unit Service Call " + "\n" +
+                "Service Address= " + serviceAddress + "\n" +
+                "Problem Description = " + problemDescription  + "\n" +
+                "Reported Date = " + reportedDate + "\n" +
+                "Resolved Date = " + resolvedDateString + "\n" +
+                "Resolution = " + resolutionString + "\n" +
+                "Fee = " + feeString ;
+
+    }
+}
